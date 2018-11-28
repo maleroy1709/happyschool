769c93f657a456a248a394b79a9a60f93fec783a
Supermanu <manueltondeur@gmail.com>
2018-11-28 21:59:12 +0100

    schedule_change: Add remote capabilities

--- schedule_change/views.py
+++ schedule_change/views.py
@@ -18,6 +18,7 @@
 # along with HappySchool.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
+import requests
 
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
@@ -28,7 +29,7 @@ from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 from django_filters import rest_framework as filters
 
-from core.views import BaseModelViewSet, BaseFilters
+from core.views import BaseModelViewSet, BaseFilters, get_core_settings
 from core.utilities import get_menu
 from core.email import send_email
 
@@ -38,6 +39,8 @@ from .serializers import ScheduleChangeSettingsSerializer, ScheduleChangeSeriali
 
 def get_settings():
     settings_schedule = ScheduleChangeSettingsModel.objects.first()
+    # Ensure core settings is created.
+    get_core_settings()
     if not settings_schedule:
         # Create default settings.
         settings_schedule = ScheduleChangeSettingsModel.objects.create().save()
@@ -87,6 +90,15 @@ class ScheduleChangeViewSet(BaseModelViewSet):
         super().perform_create(serializer)
         change = serializer.save()
         self.notify_email(change, email_general, email_substitute, "Nouveau changement")
+        if get_settings().copy_to_remote:
+            core_settings = get_core_settings()
+            remote_url = core_settings.remote
+            # Ensure a slash at the end.
+            if remote_url[-1] != '/':
+                remote_url += '/'
+            remote_token = core_settings.remote_token
+
+            requests.post(remote_url + 'schedule_change/api/schedule_change/')
 
 
     def perform_update(self, serializer):
